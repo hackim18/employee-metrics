4c186f886c7101a0b1cd972a95454ab9035fb489
Hackim18 <khakim668@gmail.com>
2024-06-28 05:23:31 +0700

    feat: nest g controller, module, service employees

--- employee-metrics/src/app.module.ts
+++ employee-metrics/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ElasticsearchModule } from './elasticsearch/elasticsearch.module';
+import { EmployeesController } from './employees/employees.controller';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [ElasticsearchModule],
-  controllers: [AppController],
+  imports: [ElasticsearchModule, EmployeesModule],
+  controllers: [AppController, EmployeesController],
   providers: [AppService],
 })
 export class AppModule {}
